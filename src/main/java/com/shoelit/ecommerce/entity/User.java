f0575eb76a1357ed299c7e03284aabbc47e872cf
Lostkyd <ronnkev123@gmail.com>
2024-12-13 06:38:10 +0000

    initializing eCommerce project

---
+++ src/main/java/com/shoelit/ecommerce/entity/User.java
@@ -0,0 +1,102 @@
+
+package com.shoelit.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String email;
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private String gender;
+    private Integer age;
+    private String password;
+    
+    public User(){
+        
+    }
+        public User(String email, String firstname, String middlename, String lastname, String gender, Integer age, String password) {
+        this.email = email;
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.age = age;
+        this.password = password;
+    }
+        
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/shoelit/ecommerce/repository/UserRepo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.shoelit.ecommerce.repository;
+
+import com.shoelit.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Ronn Kevin Rubio
+ */
+public interface UserRepo extends JpaRepository<User, Long>{
+    
+}
